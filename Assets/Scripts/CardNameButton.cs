0cc936efc94bcb1b654eb9f16c9ae80a228bafd7
Jackson <gamedev@melodatron.com>
2017-09-24 17:39:40 +1000

    Initial Commit

---
+++ Assets/Scripts/CardNameButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardNameButton : MonoBehaviour
+{
+    public InputField input_field;
+    public DeckbrewRequester requester;
+    public Text output_field;
+
+    public void HandleRequest()
+    {
+        string search_string = input_field.text;
+
+        requester.DoRequest(OnRequestComplete, search_string);
+    }
+
+    public void OnRequestComplete(bool was_success, string response)
+    {
+        output_field.text = response;
+    }
+}
---
+++ Assets/Scripts/DeckbrewRequester.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DeckbrewRequester : MonoBehaviour
+{
+    public delegate void CallBack(bool was_success, string response);
+
+    public void DoRequest(CallBack callback, string card_name)
+    {
+        Debug.Assert(callback != null, "CallBack cannot be null");
+        StartCoroutine(GetResponse(callback, card_name));
+    }
+
+    private IEnumerator GetResponse(CallBack callback, string card_name)
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://api.deckbrew.com/mtg/cards?name=" + card_name);
+        yield return www.Send();
+ 
+        string response = "";
+        bool was_success = false;
+
+        if(www.isNetworkError) {
+            response = www.error;
+        }
+        else {
+            // Show results as text
+            response = www.downloadHandler.text;
+ 
+            // Or retrieve results as binary data
+            // byte[] results = www.downloadHandler.data;
+        }
+
+        callback(was_success, response);
+    }
+}
